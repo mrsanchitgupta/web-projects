f93f07d814c7fa867b8f926066df8a65a1ae82f8
mrsanchitgupta <35406729+mrsanchitgupta@users.noreply.github.com>
2019-07-25 11:08:21 +0530

    Add files via upload

---
+++ course registeration system/connfile.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "table";
+
+// Create connection
+$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
