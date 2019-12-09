4eb3f1cbe12994096e5345091742c73f5ce88cc1
Priyeshkkumar <priyeshkkumar@gmail.com>
2019-12-09 15:01:40 +0530

    finally a DBMS project

--- course registeration system/login.php
+++ course registeration system/login.php
@@ -1,71 +1,38 @@
-
 <!DOCTYPE html>
-
 <html>
+
 <head>
-<meta name="viewport" content="device-width",initial-scale="1.0">
-<title>Login or Signup</title>
-<link rel="stylesheet" href="w3.css">
-<link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css"> 
 
-<style>
-html,h1,h2,h3,h4,h5,h6,body
-{
-font-family: 'Playfair Display SC', serifaa;
-font-size:18px;
-text-shadow:4px 4px 4px #aaa;
- }
-.w3-hover-navy-peony
-{color:#fff;background-color:#223a5e}
-</style>
+  <meta charset="UTF-8">
+
+  <title> Log-in</title>
+
+  <link rel='stylesheet' href='http://codepen.io/assets/libs/fullpage/jquery-ui.css'>
+
+    <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+
+</head>
+
 <body>
-<div class="w3-row">
-   <div class="w3-col l8 s12">
-	<div class="w3-card" >
-	<img src="images/5.jpg " style="width:100%;height:600px">
-	</div>
 
-   </div>
+  <div class="login-card">
+    <h1>
+   <img src="images/7.gif" alt="logo" align="middle" class="w3-image"  width=100px"><br>
+   Log-in</h1><br>
+  <form name="login_form" action="retrieval.php" method ="post" enctype="multipart/form-data">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
+    <input type="submit" name="login" class="login login-submit" value="Submit"  onclick ="returnvalidation();" >
+  </form>
 
-
-   <div class="w3-col l4 s12">
-
-	<div class="w3-container">
-	<div class="w3-card-4"style="width:90%" >
-	<div class="w3-container w3-flat-belize-hole"><h2>Login</h2>
-	</div>
-
-
-	  <img src="images/avlogin.png" style="width:100%;height:80%">
-
-<form  name="_form" action="retrieval.php" method ="post" enctype="multipart/form-data">
-
-
-      <div clss="w3-container">
-  User Id:<br>
-
-  <input type= "txt" name ="username" required><br>
-
-  Password:<br>
-  <input type ="password" name="password" required><br>
-   
-    <Input type="submit" value="submit" class="w3-hover-text-purple" onclick ="returnvalidation();">
-
-<div class="w3-container w3-center w3-grey w3-hover-text-purple">
-<a href="signup.php"><h2>New User Register here</h2></a>
-</div>
- </div>
- </div>
- </div>
-
-</form>
-<br>
-
-
+  <div class="login-help">
+    <a href="signup.php">Register</a> 
+  </div>
 </div>
 
+<!-- <div id="error"><img src="https://dl.dropboxusercontent.com/u/23299152/Delete-icon.png" /> Your caps-lock is on.</div> -->
 
+  <script src='http://codepen.io/assets/libs/fullpage/jquery_and_jqueryui.js'></script>
 
 </body>
 
--- course registeration system/uploadcheck.php
+++ course registeration system/uploadcheck.php
@@ -48,13 +48,14 @@ if ($_FILES["myfile"]["size"]&&$_FILES["signature"]["size"] > 500000) {
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)&&move_uploaded_file($_FILES["signature"]["tmp_name"],$sign_file)) {
-        //echo "The file ". basename( $_FILES["myfile"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
 } 
+else {
+    move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)&&move_uploaded_file($_FILES["signature"]["tmp_name"],$sign_file) ;
+        //echo "The file ". basename( $_FILES["myfile"]["name"]). " has been uploaded.";
+    } /*else {
+        echo "Sorry, there was an error uploading your file.";
+    }*/
+
 
 
 ?>
